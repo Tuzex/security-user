318f9db4ace8076109cf6f1fa79d42c0a498ac57
Edo <iam@edobarnas.com>
2020-02-08 17:12:16 +0100

    AnonymousUser can now be transformed to string.

--- src/AnonymousUser.php
+++ src/AnonymousUser.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace Tuzex\Security\User;
 
 use Tuzex\Security\User\Behavior\Identifiable;
+use Tuzex\Security\User\Behavior\Stringable;
 
-final class AnonymousUser implements User, Identifiable
+final class AnonymousUser implements User, Identifiable, Stringable
 {
     private string $id;
 
---
+++ src/Behavior/Stringable.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tuzex\Security\User\Behavior;
+
+interface Stringable
+{
+    public function __toString(): string;
+}
